9c3980347827775d8549c2f92c25a589eef05f80
Leon <leonwheeler08@gmail.com>
2021-08-07 10:26:18 +0100

    Created axiosFetcher that accepts a given url

---
+++ src/fetchers/axiosFetcher.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+
+const axiosFetcher = async (url, params) => {
+  try {
+    const response = await axios.get(url, { params });
+
+    return response.search;
+  } catch (err) {
+    console.error(err.message);
+    throw new Error("Unable to retrieve data");
+  }
+};
+
+module.exports = axiosFetcher;
---
+++ src/resolvers/index.js
@@ -0,0 +1,7 @@
+const resolvers = {
+  Query: {
+    movies,
+  },
+};
+
+module.exports = resolvers;
